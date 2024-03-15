51be1492758f52ff4d982ec7db77f7d592f02ec5
Eduardo Vergara� <eduver_san@hotmail.com>
2024-03-15 14:08:58 +0000

    feat: :sparkles: Add hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+import animations from '@midudev/tailwind-animations';
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        accent: 'var(--color-accent)',
+      },
+      screens: {
+        xs: '360px',
+        ...defaultTheme.screens,
+        '3xl': '1650px',
+      },
+    },
+  },
+  plugins: [animations],
+};
